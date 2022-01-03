33e9f377973125d4210de51ea60e53d662e1610a
Ted0527 <froshowling@gmail.com>
2022-01-03 16:51:08 +0900

    wip

--- quntum/shops/serializers.py
+++ quntum/shops/serializers.py
@@ -13,7 +13,7 @@ class CategorySerializer(ModelSerializer):
         shop = data.shop_set.all()
         return shop.values()
 
-class ShopSerializer(ModelSerializer):
+class ShopSerializer(serializers.ModelSerializer):
     customer_name = serializers.SerializerMethodField()
     customer_total = serializers.SerializerMethodField()
 
@@ -24,24 +24,45 @@ class ShopSerializer(ModelSerializer):
     def get_customer_name(self, data):
         return set([visitor.customer.name for visitor in data.visited_set.all()])
     
-        # visitors = [i.visited_set.all() for i in data]
-        # customer_id = set([visitor.filter() for visitor in visitors])
-        # customer = Customer.objects.get(id=customer_id)
-        # result=[set([visitors.customer.name for visitors in shop.visited_set.all()]) for shop in data]
-        # return result
+        # customer = set([visitor.customer.name for visitor in data.visited_set.all()])
+        # return customer
+        # visitors = Visited.objects.filter(customer_id = data.id).select_related('customer')
+        # customers = [[a.customer.name for a in i] for i in visitors]
+        # result=[[visitors.customer.name for visitors in shop.visited_set.all()] for shop in data]
 
     def get_customer_total(self, data):
         customer = Visited.objects.filter(shop_id = data.id).count()
         return customer
+    #     visitors = Visited.objects.filter(shop_id = data.id)
+    #     customers = [i.customer_id for i in visitors]
+    #     return len(set(customers))
     
 class CustomerSerializer(ModelSerializer):
+    visited_shops = serializers.SerializerMethodField()
 
     class Meta:
         model = Customer
         fields = '__all__'
+    
+    def get_visited_shops(self, data):
+        return Visited.objects.filter(customer_id = data.id).values()
+        # print(data)
+        # results = [{
+        #     'id' : i.id,
+        #     'shop_id' : i.shop_id,
+        #     'visited_time' : i.visited_time,
+        #     'customer_id' : i.customer_id,
+        # }for i in data.visited_set.all()]
+        
+        # return results
+        
+        # shops_name = [customer.shop.name for customer in data.visited_set.all()]
+        # visited_time = [i.visited_time for i in data.visited_set.all()]
+        # return visited_time, shops_name
+
+
 
 class VisitedSerializer(ModelSerializer):
-    # visited_shop = serializers.SerializerMethodField()
 
     class Meta:
         model = Visited
