f39fccdf9b8090238a9846d56c96b506bdab95ee
Ted0527 <froshowling@gmail.com>
2022-01-19 14:37:44 +0900

    Add: url-router 추가

--- MovieListAPI-Django/watchlist_app/api/urls.py
+++ MovieListAPI-Django/watchlist_app/api/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from watchlist_app.api.views import StreamPlatformAV, StreamPlatformDetailAV, WatchListAV, WatchDetailAV, ReviewList, ReviewDetail, StreamPlatformViewSet
+from watchlist_app.api.views import StreamPlatformAV, StreamPlatformDetailAV, WatchListAV, WatchDetailAV, ReviewList, ReviewDetail, StreamPlatformViewSet,\
+    UserViewSet, PointViewSet, PointInfoViewSet
+
+
 # from watchlist_app.api.views import MovieListAV, MovieDetailAV
 # from watchlist_app.api.views import movie_list, movie_details
 
 
 router = DefaultRouter()
 router.register('stream', StreamPlatformViewSet, basename='platform')
+router.register('users', UserViewSet, basename='users')
+router.register('points', PointViewSet, basename='points')
+router.register('pointinfo', PointInfoViewSet, basename='pointinfo')
 
 urlpatterns = [
     # path('', MovieListAV.as_view(), name='movie-list'),
