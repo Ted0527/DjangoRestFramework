c073b68047e9675213405288d76f559f68821885
Ted0527 <froshowling@gmail.com>
2022-02-09 21:01:52 +0900

    user registration, login success

--- refactoring/Brokurly_v2/users/admin.py
+++ refactoring/Brokurly_v2/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from users.models import CustomAccounts
 
 # Register your models here.
+admin.site.register(CustomAccounts)
+
+# @admin.register(CustomAccounts)
+# class CustomAccountsAdmin(admin.ModelAdmin):
+#   list_display = ['id', 'email', 'password']
+#   list_display_links = ['id', 'email', 'password']
+#   search_fields = ['shop_name', ]
