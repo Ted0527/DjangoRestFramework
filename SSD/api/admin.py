09299a2ec2bad841f61061a15af5056b058aab03
Ted0527 <froshowling@gmail.com>
2022-02-20 20:16:41 +0900

    clean up the repo

--- SSD/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from api.models import Point, Reward, TimeTable
-# Register your models here.
-
-admin.site.register(Point)
-admin.site.register(Reward)
-admin.site.register(TimeTable)
